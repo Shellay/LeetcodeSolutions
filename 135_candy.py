d1dd444a66f11bcef9c85001521ffc79ecf70d0a
Shellay Lee <shellaylee@hotmail.com>
2015-04-27 21:42:07 +0200

    Use one pass...
    Clearer

--- 135_candy.py
+++ 135_candy.py
@@ -1,5 +1,6 @@
 import pdb
 
+# Problem description:
 # There are N children standing in a line. Each child is assigned a rating value.
 
 # You are giving candies to these children subjected to the following requirements:
@@ -8,7 +9,7 @@ import pdb
 # Children with a higher rating get more candies than their neighbors.
 # What is the minimum candies you must give?
 
-# Points:
+# Keypoints:
 # - Suppose the first child has 1 candy;
 # - Iterate pairwise (x,y) through the line of children;
 #   -> if x.value < y.value, give y 1 more candy than x has;
@@ -23,26 +24,33 @@ import pdb
 # *. Along iteration of children-rate-values, deliver a gap
 #    each time when descending;
 
-def find_zigs(seq):
-    """ zigs is a sequence of [a, b, a, b, ...], where
-    [a:b] is ascending part, [b:a] is descending part.
-    i.e. seq1[a] is peak, seq1[b] is bottom.
-    """
+def candies(seq):
     seq1 = [0] + seq + [0]
-    asc = True ; last_turning = -1
+    cand = [0] * len(seq1)
+    asc = True ; prv = 1
     for i in range(1, len(seq1)-1): 
         if asc:
             if seq1[i] > seq1[i+1]:
-                asc = not asc
-                spl.append(i)
+                asc = not asc 
+                cand[prv] = 1
+                for j in range(prv, i+1): # Climb forwards...
+                    cand[j+1] = cand[j] + \
+                                (0 if seq1[j] == seq1[j+1] else 1)
+                prv = i
         else:
             if seq1[i] < seq1[i+1]:
-                asc = not asc
-                spl.append(i)
-    return seq1, [0] + spl + [len(seq1)-1]
+                asc = not asc 
+                cand[i] = 1
+                for j in range(i-1, prv, -1): # Climb backwords...
+                    cand[j] = cand[j+1] + \
+                              (0 if seq1[j] == seq1[j+1] else 1)
+                cand[prv] = max(cand[prv],
+                                cand[prv+1]+1)
+                prv = i
+    return cand
 
 seq = [10, 11, 14, 9, 18, 13, 10, 5, 12, 15]
 
-seq1, zigs = find_zigs(seq)
+c = candies(seq)
 
-c = candies(seq1, zigs)
+c = c[1:-1]
